197e2f3a1165a354a5c54886bf8d798691655dd9
Matteo Greatti <matteo.greatti@iteffe.com>
2017-07-28 15:02:03 +0200

    Added ip parameter to arp_scan function

--- lib/arp_scan.rb
+++ lib/arp_scan.rb
@@ -13,13 +13,14 @@ end
 #
 # target - CIDR block
 # iface  - Ethernet interface to use
+# ip     - Source ip for request
 #
 # Examples
 #
-#   arp_scan('192.168.0.0/24', 'eth0')
+#   arp_scan('192.168.0.0/24', 'eth0', '192.168.1.1')
 #
 # Returns scan result
-def arp_scan(target, iface) 
+def arp_scan(target, iface, ip) 
   if !Process.euid.zero?
     usage
     return nil
@@ -28,7 +29,8 @@ def arp_scan(target, iface)
 
     puts "Discovering: #{target}, on interface: #{iface}"
 
-    response_scan = PacketFu::Utils.arp(target, :iface => iface)
+    puts ip
+    response_scan = PacketFu::Utils.arp(target, :iface => iface, :ip_saddr => ip)
 
     return response_scan
   end
